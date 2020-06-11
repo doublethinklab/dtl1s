71de95ba0343303281c4ba7aac1648c501a04559
Fly Chang <leo424y@gmail.com>
2020-06-11 13:58:30 +0800

    add_index

--- db/migrate/20200041535620_create_pages.rb
+++ db/migrate/20200041535620_create_pages.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatePages < ActiveRecord::Migration[6.0]
   def change
     create_table :pages do |t|
@@ -13,9 +15,16 @@ class CreatePages < ActiveRecord::Migration[6.0]
       t.string :score
       t.timestamps
     end
-    [:uid, :uname, :pid, :ptitle, :ptype, :ptime, :mtime, :url, :platform, :score].each do |a|
-      add_index :pages, a 
-    end
 
+    add_index :pages, :uid
+    add_index :pages, :uname
+    add_index :pages, :pid
+    add_index :pages, :ptitle
+    add_index :pages, :ptype
+    add_index :pages, :ptime
+    add_index :pages, :mtime
+    add_index :pages, :url
+    add_index :pages, :platform
+    add_index :pages, :score
   end
 end
