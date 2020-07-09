9621933603ce481b811392489221e9b1e28fe346
Fly Chang <leo424y@gmail.com>
2020-07-09 11:28:29 +0800

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :protect
+  # before_action :protect
 
   def protect
     @ips = ENV['IPOK'].split(',')
